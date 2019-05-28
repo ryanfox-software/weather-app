f599f9fea0980051e652a117121e136c279b1eca
ryanfox-software <>
2019-05-28 11:16:24 -0500

    Added 5 day forecast

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,23 +1,72 @@
 import React from "react";
 
 class Titles extends React.Component {
+
+/*2,4,6,
+  10,12,14,
+  18,20,22,
+  26,28,30
+  34,36,38
+  6:00am, 12:00 noon, 6:00pm
+*/
   render(){
+    var mydata = this.props.data;
+    //var temps = ["Weather Forecast not established. Please select city"];
+    var temps = [15];
+    for(let i = 0; i < 15; i++){
+      temps[i] = "";
+      if(this.props.data != null){
+          temps[i] = this.props.data.list[i+2%8].main.temp;
+      }
+    }
+
+
     return (
       <div className="forecast">
       <div className="forecastpanel1">
-        Current Weather Report
+        Temperature at
+        6:00 am
+        {temps[0]}
+        12:00 pm
+        {temps[1]}
+        6:00 pm
+        {temps[2]}
       </div>
       <div className="forecastpanel2">
-        Current Weather Report
+      Temperature at
+      6:00 am
+      {temps[3]}
+      12:00 pm
+      {temps[4]}
+      6:00 pm
+      {temps[5]}
       </div>
       <div className="forecastpanel3">
-        Current Weather Report
+      Temperature at
+      6:00 am
+      {temps[6]}
+      12:00 pm
+      {temps[7]}
+      6:00 pm
+      {temps[8]}
       </div>
       <div className="forecastpanel4">
-        Current Weather Report
+      Temperature at
+      6:00 am
+      {temps[9]}
+      12:00 pm
+      {temps[10]}
+      6:00 pm
+      {temps[11]}
       </div>
       <div className="forecastpanel5">
-        Current Weather Report
+      Temperature at
+      6:00 am
+      {temps[12]}
+      12:00 pm
+      {temps[13]}
+      6:00 pm
+      {temps[14]}
       </div>
       </div>
     );
